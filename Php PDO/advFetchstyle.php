26d1e9a05ad38ba1ef6598dd859f6b7ae1169ff4
mohammed-kangda <kangdamohammed551@gmail>
2022-07-10 19:25:21 +0530

    Initial Commit

---
+++ Php PDO/advFetchstyle.php
@@ -0,0 +1,26 @@
+<!-- 
+
+    Advance Fetch styles
+
+    PDO::FETCH_COLUMN
+    PDO::FETCH_GROUP
+    PDO::FETCH_UNIQUE
+    PDO::FETCH_KEY_PAIR
+    PDO::FETCH_CLASS
+
+    $esult->fetchAll(PDO::FETCH_);
+
+
+    PDO Methods:
+
+    rowCount()
+    exec()
+    lastInsertid()
+
+-->
+
+<?php 
+    
+      
+    
+?>
